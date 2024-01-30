9c45c131be0630df252dfa4833d9b2ae5079e566
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-29 22:06:08 -0500

    Change Answers to UserResponse

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Survey, Question, Answer
+from .models import Survey, Question, UserResponse
 
 admin.site.register(Survey)
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(UserResponse)
