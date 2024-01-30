86cbe8c8bb0d0404b9d4c7aea6fcc8ed57f321ba
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 01:43:24 -0500

    More linting

--- asgi.py
+++ asgi.py
@@ -1,16 +1,18 @@
 import os
-from django.core.asgi import get_asgi_application
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
 import twilio_service.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-      URLRouter(
-          twilio_service.routing.websocket_urlpatterns
-      )
-  ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(twilio_service.routing.websocket_urlpatterns)
+        ),
+    }
+)
