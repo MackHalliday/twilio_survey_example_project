542ad3e07bc21011e129953bf405472cac6ee680
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-29 22:39:32 -0500

    Very, very rough code for recieving survey response and sending next question

--- twilio_service/views.py
+++ twilio_service/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# twilio_service/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+import json
+from typing import Any
+# from django.contrib.auth.models import User
 
-# Create your views here.
+# from surveys.models import Question, UserResponse
+
+class TwilioWebhook(APIView):
+    def post(self, request: Any, *args: Any, **kwargs: Any) -> Any:
+        try:
+            twilio_data = json.loads(request.body.decode("utf-8"))
+
+            phone_number = twilio_data.get("From")
+            message_body = twilio_data.get("Body")
+
+            # user = User.objects.filter(phone_number=phone_number, is_active=True).first()
+            # current_survey_campaign = SurveyCampaign.object.filter(user, is_active=True)
+            # question = Question.objects.filter(survey=current_survey_campaign.survey_id)
+
+            # user_response = UserResponse.objects.create(respondent=user, question=question, response=message_body)
+
+            # if not user_survey_complete(user): 
+            #     send_user_next_survey_question(user)
+
+            return Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
