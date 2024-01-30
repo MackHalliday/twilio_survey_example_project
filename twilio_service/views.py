b8924c64fcd0e33ca79139bcc8247ea246e033ec
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 02:59:15 -0500

    Survey working properly - Corrected logic to save questions and responses correctly, corrected queries

--- twilio_service/views.py
+++ twilio_service/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
 from twilio.twiml.messaging_response import MessagingResponse
+from accounts.models import UserProfile
 
-from surveys.models import Question, UserResponse
+from surveys.models import Question, SurveyUser, UserResponse
 from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__NO_SURVEY_SENT,
                                      SURVEY__START)
 
@@ -16,24 +17,23 @@ class TwilioWebhook(APIView):
     def post(self, request, *args, **kwargs):
         try:
             twilio_data = parse_qs(request.body.decode("utf-8"))
+            print(twilio_data)
             incoming_msg_list = twilio_data.get("Body", [])
             incoming_msg = " ".join(incoming_msg_list)
 
-            phone_number_list = twilio_data.get("Number", [])
+            phone_number_list = twilio_data.get("From", [])
             phone_number = " ".join(phone_number_list)
 
             survey_step = request.session.get("survey_step", -1)
             answers = request.session.get("answers", [])
 
-            questions = Question.objects.filter(survey_id=1).values_list(
-                "text", flat=True
-            )
+            questions = Question.objects.filter(survey_id=1).order_by('order')
 
             if survey_step == -1:
                 response = SURVEY__START
                 request.session["survey_step"] = 0
                 request.session["answers"] = []
-            elif survey_step < len(questions):
+            elif survey_step < questions.count():
                 if survey_step == 0 and incoming_msg.lower() != "yes":
                     response = SURVEY__NO_SURVEY_SENT
                     del request.session["survey_step"]
@@ -44,23 +44,33 @@ class TwilioWebhook(APIView):
                     )
                     request.session["survey_step"] += 1
 
-                    user = User.objects.filter(
-                        phone_number=phone_number, is_active=True
-                    ).first()
-                    current_survey_campaign = SurveyCampaign.object.filter(
-                        user, is_active=True
-                    )
-                    question = Question.objects.filter(
-                        survey=current_survey_campaign.survey_id
-                    )
+                    if survey_step > 0: 
+                        last_question = survey_step -  1
+                        last_answered_question = questions[last_question]
 
-                    user_response = UserResponse.objects.create(
-                        respondent=user, question=question, response=incoming_msg
-                    )
+                        user_profile = UserProfile.objects.filter(
+                            phone_number=phone_number, 
+                        ).first()
 
+                        UserResponse.objects.create(
+                            respondent=user_profile.user, question=last_answered_question, response=incoming_msg
+                        ).save()
+                    
+                    
                     if survey_step > 0:
                         answers.append(incoming_msg)
             else:
+                last_question = survey_step -  1
+                last_answered_question = questions[last_question]
+
+                user_profile = UserProfile.objects.filter(
+                    phone_number=phone_number, 
+                ).first()
+
+                UserResponse.objects.create(
+                    respondent=user_profile.user, question=last_answered_question, response=incoming_msg
+                ).save()
+                    
                 response = SURVEY__COMPLETE
                 answers.append(incoming_msg)
                 del request.session["survey_step"]
