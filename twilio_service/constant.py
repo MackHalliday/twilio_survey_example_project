db6619e448074c0a10656b45e707687d1c83a2d0
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 02:27:30 -0500

    Create constant for survey txt

---
+++ twilio_service/constant.py
@@ -0,0 +1,3 @@
+SURVEY__START = "Would you like to take a survey? Response Yes to start."
+SURVEY__NO_SURVEY_SENT = "No survey will be sent."
+SURVEY__COMPLETE = "Thank you for completing the survey."
