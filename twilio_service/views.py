a3a8186a979481a2fc9d27d525d3df2771d1d8d7
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 02:27:17 -0500

    Sessions logic works, added constant, added logic to start survey responses

--- twilio_service/views.py
+++ twilio_service/views.py
@@ -1,34 +1,41 @@
 import traceback
 from urllib.parse import parse_qs
 
+from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
 from twilio.twiml.messaging_response import MessagingResponse
 
-from surveys.models import Question
+from surveys.models import Question, UserResponse
+from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__NO_SURVEY_SENT,
+                                     SURVEY__START)
 
 
 class TwilioWebhook(APIView):
     def post(self, request, *args, **kwargs):
         try:
             twilio_data = parse_qs(request.body.decode("utf-8"))
+            incoming_msg_list = twilio_data.get("Body", [])
+            incoming_msg = " ".join(incoming_msg_list)
 
-            incoming_msg = twilio_data.get("Body", "")
-            survey_step = twilio_data.get("survey_step", -1)
+            phone_number_list = twilio_data.get("Number", [])
+            phone_number = " ".join(phone_number_list)
+
+            survey_step = request.session.get("survey_step", -1)
+            answers = request.session.get("answers", [])
 
             questions = Question.objects.filter(survey_id=1).values_list(
                 "text", flat=True
             )
 
-            # TODO - Sessions NOT working correctly
             if survey_step == -1:
-                response = "Hello!"
+                response = SURVEY__START
                 request.session["survey_step"] = 0
                 request.session["answers"] = []
             elif survey_step < len(questions):
                 if survey_step == 0 and incoming_msg.lower() != "yes":
-                    response = "Let's go!"
+                    response = SURVEY__NO_SURVEY_SENT
                     del request.session["survey_step"]
                     del request.session["answers"]
                 else:
@@ -36,26 +43,28 @@ class TwilioWebhook(APIView):
                         f"({survey_step + 1}/{len(questions)}) {questions[survey_step]}"
                     )
                     request.session["survey_step"] += 1
+
+                    user = User.objects.filter(
+                        phone_number=phone_number, is_active=True
+                    ).first()
+                    current_survey_campaign = SurveyCampaign.object.filter(
+                        user, is_active=True
+                    )
+                    question = Question.objects.filter(
+                        survey=current_survey_campaign.survey_id
+                    )
+
+                    user_response = UserResponse.objects.create(
+                        respondent=user, question=question, response=incoming_msg
+                    )
+
                     if survey_step > 0:
-                        request.session["answers"].append(incoming_msg)
+                        answers.append(incoming_msg)
             else:
-                response = "EXIT"
-                answers = request.session["answers"]
+                response = SURVEY__COMPLETE
                 answers.append(incoming_msg)
                 del request.session["survey_step"]
                 del request.session["answers"]
-                print("Survey answers:", answers)
-
-            # user = User.objects.filter(phone_number=phone_number, is_active=True).first()
-            # current_survey_campaign = SurveyCampaign.object.filter(user, is_active=True)
-            # question = Question.objects.filter(survey=current_survey_campaign.survey_id)
-
-            # user_response = UserResponse.objects.create(respondent=user, question=question, response=message_body)
-
-            # if not user_survey_complete(user):
-            #     send_user_next_survey_question(user)
-
-            # return Response(data={"response":response}, status=status.HTTP_200_OK)
 
             twilio_response = MessagingResponse()
             twilio_response.message(response)
