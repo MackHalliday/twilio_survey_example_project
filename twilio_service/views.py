3bdfdf3b82cd895f7d3bbc27239f65b0426f3673
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 01:44:01 -0500

    Add logic for sending survey, try to get sessions to work - NOT working need to fix

--- twilio_service/views.py
+++ twilio_service/views.py
@@ -1,52 +1,50 @@
-# twilio_service/views.py
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import JsonResponse
-import json
-from typing import Any
+import traceback
 from urllib.parse import parse_qs
 
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from twilio.twiml.messaging_response import MessagingResponse
+
 from surveys.models import Question
-import traceback
 
-
-# from django.contrib.auth.models import User
-
-# from surveys.models import Question, UserResponse
 
 class TwilioWebhook(APIView):
-    def post(self, request: Any, *args: Any, **kwargs: Any) -> Any:
+    def post(self, request, *args, **kwargs):
         try:
             twilio_data = parse_qs(request.body.decode("utf-8"))
 
-            incoming_msg: str = twilio_data.get('Body', '')
-            # phone_number: str = twilio_data.get("From")
-            survey_step: int = twilio_data.get('survey_step', -1)
+            incoming_msg = twilio_data.get("Body", "")
+            survey_step = twilio_data.get("survey_step", -1)
 
-            questions = Question.objects.filter(survey_id=1).values_list('text', flat=True)
+            questions = Question.objects.filter(survey_id=1).values_list(
+                "text", flat=True
+            )
 
+            # TODO - Sessions NOT working correctly
             if survey_step == -1:
                 response = "Hello!"
-                request.session['survey_step'] = 0
-                request.session['answers'] = []
+                request.session["survey_step"] = 0
+                request.session["answers"] = []
             elif survey_step < len(questions):
-                if survey_step == 0 and incoming_msg.lower() != 'yes':
+                if survey_step == 0 and incoming_msg.lower() != "yes":
                     response = "Let's go!"
-                    del request.session['survey_step']
-                    del request.session['answers']
+                    del request.session["survey_step"]
+                    del request.session["answers"]
                 else:
-                    response = f'({survey_step + 1}/{len(questions)}) {questions[survey_step]}'
-                    request.session['survey_step'] += 1
+                    response = (
+                        f"({survey_step + 1}/{len(questions)}) {questions[survey_step]}"
+                    )
+                    request.session["survey_step"] += 1
                     if survey_step > 0:
-                        request.session['answers'].append(incoming_msg)
+                        request.session["answers"].append(incoming_msg)
             else:
                 response = "EXIT"
-                answers = request.session['answers']
+                answers = request.session["answers"]
                 answers.append(incoming_msg)
-                del request.session['survey_step']
-                del request.session['answers']
-                print('Survey answers:', answers)
+                del request.session["survey_step"]
+                del request.session["answers"]
+                print("Survey answers:", answers)
 
             # user = User.objects.filter(phone_number=phone_number, is_active=True).first()
             # current_survey_campaign = SurveyCampaign.object.filter(user, is_active=True)
@@ -54,10 +52,18 @@ class TwilioWebhook(APIView):
 
             # user_response = UserResponse.objects.create(respondent=user, question=question, response=message_body)
 
-            # if not user_survey_complete(user): 
+            # if not user_survey_complete(user):
             #     send_user_next_survey_question(user)
 
-            return Response(data={"response":response}, status=status.HTTP_200_OK)
+            # return Response(data={"response":response}, status=status.HTTP_200_OK)
+
+            twilio_response = MessagingResponse()
+            twilio_response.message(response)
+            twilio_response_str = str(twilio_response)
+
+            return HttpResponse(twilio_response_str, content_type="application/xml")
         except Exception as e:
             traceback.print_exc()
-            return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
