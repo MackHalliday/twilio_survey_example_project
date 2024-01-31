c3b1c86cdd00d21307e7054e4f21f03a37afbb05
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 16:42:36 -0500

    Update constant names, add test for no available surveys

--- twilio_service/webhook/tests.py
+++ twilio_service/webhook/tests.py
@@ -5,11 +5,15 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 from accounts.models import UserProfile
 from surveys.models import Question, Survey, SurveyUser, UserResponse
-from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__CONFIRM_START,
-                                     SURVEY__DO_NOT_SEND_SURVEY,
-                                     SURVEY__OPT_OUT_MESSAGE,
-                                     SURVEY__USER_CONFIRM_SURVEY,
-                                     TWILIO__OPT_OUT)
+from twilio_service.constant import (
+    SURVEY__COMPLETE_RESPONSE,
+    SURVEY__CONFIRM_DO_NOT_SEND_RESPONSE,
+    SURVEY__CONFIRM_START_RESPONSE,
+    SURVEY__NO_SURVEY_AVAILABLE_RESPONSE,
+    SURVEY__OPT_OUT_RESPONSE,
+    SURVEY__USER_CONFIRM_SURVEY,
+    TWILIO__OPT_OUT,
+)
 
 
 class TwilioWebhookTestCase(TestCase):
@@ -51,7 +55,26 @@ class TwilioWebhookTestCase(TestCase):
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(SURVEY__CONFIRM_START, str(response.content))
+        self.assertIn(SURVEY__CONFIRM_START_RESPONSE, str(response.content))
+
+    def test_no_available_surveys(self):
+        self.survey_user.completed = True
+        self.survey_user.save()
+
+        session = self.client.session
+        session["survey_step"] = None
+        session.save()
+
+        xml = f"From=%2B123456789&Body={SURVEY__USER_CONFIRM_SURVEY}!"
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(SURVEY__NO_SURVEY_AVAILABLE_RESPONSE, str(response.content))
 
     def test_start_survey(self):
 
@@ -85,7 +108,7 @@ class TwilioWebhookTestCase(TestCase):
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(SURVEY__DO_NOT_SEND_SURVEY, str(response.content))
+        self.assertIn(SURVEY__CONFIRM_DO_NOT_SEND_RESPONSE, str(response.content))
 
     def test_survey_complete(self):
         session = self.client.session
@@ -104,7 +127,7 @@ class TwilioWebhookTestCase(TestCase):
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(SURVEY__COMPLETE, str(response.content))
+        self.assertIn(SURVEY__COMPLETE_RESPONSE, str(response.content))
         self.assertEqual(UserResponse.objects.count(), 1)
 
     def test_user_can_opt_out(self):
@@ -121,4 +144,4 @@ class TwilioWebhookTestCase(TestCase):
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn(SURVEY__OPT_OUT_MESSAGE, str(response.content))
+        self.assertIn(SURVEY__OPT_OUT_RESPONSE, str(response.content))
