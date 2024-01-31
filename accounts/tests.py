f5bcc4c3dc1f25bf7751aac90ff917e93fce635b
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 13:25:28 -0500

    Create TODO test for feature to handle user opt-out

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from accounts.models import UserProfile
+from surveys.models import Question, Survey, SurveyUser
+from twilio_service.constant import TWILIO__OPT_OUT
+
+class OptOutTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="testuser", password="testpassword")
+        self.user_profile = UserProfile.objects.create(
+            user=self.user, phone_number="+123456789"
+        )
+        self.user_phone_number = self.user_profile.phone_number
+
+        self.client = Client()
+        self.content_type = "application/xml"
+
+    def test_user_can_opt_out(self):
+        # TODO - create class to handle user opt-out
+        pass
