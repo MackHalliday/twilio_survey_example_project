47b4cc2b6c59526c4de9c14e99d25ebc79a68811
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 22:15:39 -0500

    Refactor twilio service to send survey, make logic more readable, remove redundent logic

--- twilio_service/constant.py
+++ twilio_service/constant.py
@@ -1,3 +1,10 @@
-SURVEY__START = "Would you like to take a survey? Response Yes to start."
-SURVEY__NO_SURVEY_SENT = "No survey will be sent."
+TWILIO__OPT_IN = "START"
+TWILIO__OPT_OUT = "STOP"
+
+SURVEY__USER_CONFIRM_SURVEY = "YES"
+SURVEY__CONFIRM_START = (
+    f"Would you like to take a survey? Response {SURVEY__USER_CONFIRM_SURVEY} to start."
+)
+SURVEY__DO_NOT_SEND_SURVEY = "No survey will be sent."
 SURVEY__COMPLETE = "Thank you for completing the survey."
+SURVEY__OPT_OUT_MESSAGE = "You have been unsubscribed service."
--- twilio_service/views.py
+++ twilio_service/views.py
@@ -9,75 +9,77 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 from accounts.models import UserProfile
 from surveys.models import Question, SurveyUser, UserResponse
-from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__NO_SURVEY_SENT,
-                                     SURVEY__START)
+from twilio_service.constant import (
+    SURVEY__COMPLETE,
+    SURVEY__CONFIRM_START,
+    SURVEY__DO_NOT_SEND_SURVEY,
+    SURVEY__OPT_OUT_MESSAGE,
+    SURVEY__USER_CONFIRM_SURVEY,
+    TWILIO__OPT_OUT,
+)
 
 
 class TwilioWebhook(APIView):
     def post(self, request, *args, **kwargs):
         try:
+            # Get phone number and message from twilio data
             twilio_data = parse_qs(request.body.decode("utf-8"))
-
-            incoming_msg_list = twilio_data.get("Body", [])
-            incoming_msg = " ".join(incoming_msg_list)
 
             phone_number_list = twilio_data.get("From", [])
             phone_number = " ".join(phone_number_list)
 
-            survey_step = request.session.get("survey_step", -1)
-            answers = request.session.get("answers", [])
+            incoming_msg_list = twilio_data.get("Body", [])
+            incoming_msg = " ".join(incoming_msg_list)
 
-            questions = Question.objects.filter(survey_id=1).order_by("order")
+            # Get user by phone number
+            user_profile = UserProfile.objects.filter(
+                phone_number=phone_number,
+            ).first()
 
-            if survey_step == -1:
-                response = SURVEY__START
-                request.session["survey_step"] = 0
-                request.session["answers"] = []
-            elif survey_step < questions.count():
-                if survey_step == 0 and incoming_msg.lower() != "yes":
-                    response = SURVEY__NO_SURVEY_SENT
-                    del request.session["survey_step"]
-                    del request.session["answers"]
-                else:
-                    response = (
-                        f"({survey_step + 1}/{len(questions)}) {questions[survey_step]}"
-                    )
-                    request.session["survey_step"] += 1
-
-                    if survey_step > 0:
-                        last_question = survey_step - 1
-                        last_answered_question = questions[last_question]
-
-                        user_profile = UserProfile.objects.filter(
-                            phone_number=phone_number,
-                        ).first()
-
-                        UserResponse.objects.create(
-                            respondent=user_profile.user,
-                            question=last_answered_question,
-                            response=incoming_msg,
-                        ).save()
-
-                    if survey_step > 0:
-                        answers.append(incoming_msg)
-            else:
-                last_question = survey_step - 1
-                last_answered_question = questions[last_question]
-
-                user_profile = UserProfile.objects.filter(
-                    phone_number=phone_number,
-                ).first()
-
-                UserResponse.objects.create(
-                    respondent=user_profile.user,
-                    question=last_answered_question,
-                    response=incoming_msg,
-                ).save()
-
-                response = SURVEY__COMPLETE
-                answers.append(incoming_msg)
+            # If 'opt-out' then do
+            if incoming_msg == TWILIO__OPT_OUT:
+                user_profile.update(active=False)
+                response = SURVEY__OPT_OUT_MESSAGE
                 del request.session["survey_step"]
-                del request.session["answers"]
+
+            # Else send survey
+            else:
+                survey_step = request.session.get("survey_step", None)
+
+                user_current_survey = SurveyUser.objects.filter(
+                    user=user_profile.user, is_active=True
+                ).order_by("sent_at")[0]
+                questions = Question.objects.filter(
+                    survey_id=user_current_survey
+                ).order_by("order")
+
+                if not survey_step:
+                    response = SURVEY__CONFIRM_START
+                    request.session["survey_step"] = 0
+
+                elif (
+                    survey_step == 0
+                    and incoming_msg.lower() != SURVEY__USER_CONFIRM_SURVEY
+                ):
+                    response = SURVEY__DO_NOT_SEND_SURVEY
+                    del request.session["survey_step"]
+
+                else:
+                    last_answered_question = questions[survey_step - 1]
+
+                    UserResponse.objects.create(
+                        respondent=user_profile.user,
+                        question=last_answered_question,
+                        response=incoming_msg,
+                    ).save()
+
+                    if survey_step > len(questions):
+                        response = f"({survey_step + 1}/{len(questions)}) {questions[survey_step]}"
+                        request.session["survey_step"] += 1
+
+                    else:
+                        response = SURVEY__COMPLETE
+                        del request.session["survey_step"]
 
             twilio_response = MessagingResponse()
             twilio_response.message(response)
