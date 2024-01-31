c58ab0363fa35fe00697fa19c0facfcb5c4414c9
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 13:25:53 -0500

    Correct constants for survey - should be lower case

--- twilio_service/constant.py
+++ twilio_service/constant.py
@@ -1,7 +1,7 @@
-TWILIO__OPT_IN = "START"
-TWILIO__OPT_OUT = "STOP"
+TWILIO__OPT_IN = "start"
+TWILIO__OPT_OUT = "stop"
 
-SURVEY__USER_CONFIRM_SURVEY = "YES"
+SURVEY__USER_CONFIRM_SURVEY = "yes"
 SURVEY__CONFIRM_START = (
     f"Would you like to take a survey? Response {SURVEY__USER_CONFIRM_SURVEY} to start."
 )
