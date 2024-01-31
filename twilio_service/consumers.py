cd555c602671bb533ff9d53feec1a0216ad19a67
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 13:34:21 -0500

    Remove comments

--- twilio_service/consumers.py
+++ twilio_service/consumers.py
@@ -13,8 +13,5 @@ class SurveyConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
-        # Save response ?
-        # Next question ?
-
+        
         await self.send(text_data=json.dumps({"message": message}))
