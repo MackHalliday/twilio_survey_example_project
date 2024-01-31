cd44741e6066e056c9b3eef3698dc77eb8766b29
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 15:19:50 -0500

    Create folder for websocket file

---
+++ twilio_service/webhook/tests.py
@@ -0,0 +1,124 @@
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from django.urls import reverse
+from twilio.twiml.messaging_response import MessagingResponse
+
+from accounts.models import UserProfile
+from surveys.models import Question, Survey, SurveyUser, UserResponse
+from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__CONFIRM_START,
+                                     SURVEY__DO_NOT_SEND_SURVEY,
+                                     SURVEY__OPT_OUT_MESSAGE,
+                                     SURVEY__USER_CONFIRM_SURVEY,
+                                     TWILIO__OPT_OUT)
+
+
+class TwilioWebhookTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="testuser", password="testpassword")
+        self.user_profile = UserProfile.objects.create(
+            user=self.user, phone_number="+123456789"
+        )
+        self.user_phone_number = self.user_profile.phone_number
+
+        self.survey = Survey.objects.create(survey_name="Test Survey", version=1)
+        self.question_1 = Question.objects.create(
+            text="Question 1?", survey=self.survey, order=1
+        )
+        self.question_2 = Question.objects.create(
+            text="Question 2?", survey=self.survey, order=2
+        )
+        self.question_3 = Question.objects.create(
+            text="Question 3?", survey=self.survey, order=3
+        )
+
+        self.survey_user = SurveyUser.objects.create(
+            survey=self.survey, user=self.user, completed=False
+        )
+
+        self.client = Client()
+        self.webhook_survey_url = reverse("webhook-survey")
+        self.content_type = "application/xml"
+
+    def test_user_can_opt_in(self):
+
+        xml = f"From=%2B123456789&Body=Hi!"
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(SURVEY__CONFIRM_START, str(response.content))
+
+    def test_start_survey(self):
+
+        session = self.client.session
+        session["survey_step"] = 0
+        session.save()
+
+        xml = f"From=%2B123456789&Body={SURVEY__USER_CONFIRM_SURVEY}"
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.question_1.text, str(response.content))
+
+    def test_no_survey_sent(self):
+
+        session = self.client.session
+        session["survey_step"] = 0
+        session.save()
+
+        xml = "From=%2B123456789&Body=No"
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(SURVEY__DO_NOT_SEND_SURVEY, str(response.content))
+
+    def test_survey_complete(self):
+        session = self.client.session
+        session["survey_step"] = 3
+        session.save()
+
+        twilio_response = MessagingResponse()
+        twilio_response.message("Survey complete.")
+
+        xml = "From=%2B123456789&Body=Response to last question asked."
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(SURVEY__COMPLETE, str(response.content))
+        self.assertEqual(UserResponse.objects.count(), 1)
+
+    def test_user_can_opt_out(self):
+        session = self.client.session
+        session["survey_step"] = 0
+        session.save()
+
+        xml = f"From=%2B123456789&Body={TWILIO__OPT_OUT}"
+
+        response = self.client.post(
+            self.webhook_survey_url,
+            data=xml,
+            content_type=self.content_type,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(SURVEY__OPT_OUT_MESSAGE, str(response.content))
---
+++ twilio_service/webhook/views.py
@@ -0,0 +1,88 @@
+import traceback
+from urllib.parse import parse_qs
+
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from twilio.twiml.messaging_response import MessagingResponse
+
+from accounts.models import UserProfile
+from surveys.models import Question, SurveyUser, UserResponse
+from twilio_service.constant import (SURVEY__COMPLETE, SURVEY__CONFIRM_START,
+                                     SURVEY__DO_NOT_SEND_SURVEY,
+                                     SURVEY__OPT_OUT_MESSAGE,
+                                     SURVEY__USER_CONFIRM_SURVEY,
+                                     TWILIO__OPT_OUT)
+
+
+class TwilioWebhook(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            twilio_data = parse_qs(request.body.decode("utf-8"))
+
+            phone_number_list = twilio_data.get("From", [])
+            phone_number = " ".join(phone_number_list)
+
+            incoming_msg_list = twilio_data.get("Body", [])
+            incoming_msg = " ".join(incoming_msg_list)
+
+            user_profile = UserProfile.objects.get(phone_number=phone_number)
+
+            if incoming_msg.lower() == TWILIO__OPT_OUT:
+                user_profile.active = False
+                user_profile.save()
+
+                response = SURVEY__OPT_OUT_MESSAGE
+                del request.session["survey_step"]
+
+            else:
+                survey_step = request.session.get("survey_step", None)
+
+                user_current_survey = SurveyUser.objects.filter(
+                    user=user_profile.user, completed=False
+                ).order_by("sent_at")[0]
+
+                questions = Question.objects.filter(
+                    survey_id=user_current_survey.id
+                ).order_by("order")
+
+                if survey_step is None:
+                    response = SURVEY__CONFIRM_START
+                    request.session["survey_step"] = 0
+
+                elif (
+                    survey_step == 0
+                    and incoming_msg.lower() != SURVEY__USER_CONFIRM_SURVEY
+                ):
+                    response = SURVEY__DO_NOT_SEND_SURVEY
+                    del request.session["survey_step"]
+
+                else:
+                    if survey_step > 0:
+                        last_answered_question = questions[survey_step - 1]
+                        UserResponse.objects.create(
+                            respondent=user_profile.user,
+                            question=last_answered_question,
+                            response=incoming_msg,
+                        ).save()
+
+                    if survey_step < len(questions):
+                        response = f"({survey_step + 1}/{len(questions)}) {questions[survey_step].text}"
+                        request.session["survey_step"] += 1
+
+                    else:
+                        user_current_survey.completed = True
+                        user_current_survey.save()
+                        response = SURVEY__COMPLETE
+                        del request.session["survey_step"]
+
+            twilio_response = MessagingResponse()
+            twilio_response.message(response)
+            twilio_response_str = str(twilio_response)
+
+            return HttpResponse(twilio_response_str, content_type="application/xml")
+        except Exception as e:
+            traceback.print_exc()
+            return JsonResponse(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
