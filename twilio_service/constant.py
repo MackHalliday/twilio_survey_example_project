a271f02a0623c742f04788cfb3547b5ac9510e01
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 16:42:11 -0500

    Update constant names

--- twilio_service/constant.py
+++ twilio_service/constant.py
@@ -2,9 +2,10 @@ TWILIO__OPT_IN = "start"
 TWILIO__OPT_OUT = "stop"
 
 SURVEY__USER_CONFIRM_SURVEY = "yes"
-SURVEY__CONFIRM_START = (
+SURVEY__COMPLETE_RESPONSE = "Thank you for completing the survey."
+SURVEY__CONFIRM_START_RESPONSE = (
     f"Would you like to take a survey? Response {SURVEY__USER_CONFIRM_SURVEY} to start."
 )
-SURVEY__DO_NOT_SEND_SURVEY = "No survey will be sent."
-SURVEY__COMPLETE = "Thank you for completing the survey."
-SURVEY__OPT_OUT_MESSAGE = "You have been unsubscribed this service."
+SURVEY__CONFIRM_DO_NOT_SEND_RESPONSE = "No survey will be sent."
+SURVEY__NO_SURVEY_AVAILABLE_RESPONSE = "All available surveys have been completed."
+SURVEY__OPT_OUT_RESPONSE = "You have been unsubscribed this service."
