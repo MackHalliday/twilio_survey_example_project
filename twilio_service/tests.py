6f308d85311b7c1e209aa962ba822ead668925dc
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-30 23:07:17 -0500

    Remove mocks - no needed. Add user_profile, questions, survey_user, and client. Corrected sessions

--- twilio_service/tests.py
+++ twilio_service/tests.py
@@ -1,80 +1,91 @@
 from unittest.mock import patch
 
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
+import phonenumbers
 from twilio.twiml.messaging_response import MessagingResponse
 
 from accounts.models import UserProfile
-from surveys.models import Question, Survey, UserResponse
+from surveys.models import Question, Survey, SurveyUser, UserResponse
+from twilio_service.constant import (
+    SURVEY__DO_NOT_SEND_SURVEY,
+    SURVEY__USER_CONFIRM_SURVEY,
+)
 
 
 class TwilioWebhookTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username="testuser", password="testpassword")
+        self.user_profile = UserProfile.objects.create(
+            user=self.user, phone_number="+13092125377"
+        )
+        self.user_phone_number = self.user_profile.phone_number
+
         self.survey = Survey.objects.create(survey_name="Test Survey", version=1)
+        self.question_1 = Question.objects.create(
+            text="Question 1?", survey=self.survey, order=1
+        )
+        self.question_2 = Question.objects.create(
+            text="Question 2?", survey=self.survey, order=2
+        )
+        self.question_3 = Question.objects.create(
+            text="Question 3?", survey=self.survey, order=3
+        )
 
-    @patch("twilio_service.views.parse_qs")
-    def test_start_survey(self, mock_parse_qs):
-        mock_parse_qs.return_value = {"Body": ["yes"], "From": ["+1234567890"]}
-        twilio_response = MessagingResponse()
-        twilio_response.message("Hello!")
+        self.survey_user = SurveyUser.objects.create(
+            survey=self.survey, user=self.user, completed=False
+        )
 
-        with patch.object(
-            MessagingResponse, "__str__", return_value=str(twilio_response)
-        ):
-            response = self.client.post(
-                reverse("income-message"),
-                data={
-                    "Body": "Yes",
-                    "From": "+1234567890",
-                },
-            )
+        self.client = Client()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Hello!", str(response.content))
+    def test_start_survey(self):
 
-    @patch("twilio_service.views.parse_qs")
-    def test_no_survey_sent(self, mock_parse_qs):
-        mock_parse_qs.return_value = {"Body": ["no"], "From": ["+1234567890"]}
-        twilio_response = MessagingResponse()
-        twilio_response.message("No survey sent.")
+        session = self.client.session
+        session["survey_step"] = 1
+        session.save()
 
-        with patch.object(
-            MessagingResponse, "__str__", return_value=str(twilio_response)
-        ):
-            response = self.client.post(
-                reverse("income-message"),
-                data={
-                    "Body": "no",
-                    "From": "+1234567890",
-                },
-            )
+        response = self.client.post(
+            reverse("income-message"),
+            data={
+                "Body": [SURVEY__USER_CONFIRM_SURVEY],
+                "From": [str(self.user_phone_number)],
+            },
+        )
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn("No survey sent.", str(response.content))
+        self.assertIn(self.question_1.text, str(response.content))
 
-    @patch("twilio_service.views.parse_qs")
-    def test_survey_complete(self, mock_parse_qs):
-        mock_parse_qs.return_value = {"Body": ["answer"], "From": ["+1234567890"]}
-        UserProfile.objects.create(phone_number="+1234567890", user=self.user)
-        Question.objects.create(survey=self.survey, text="Question 1")
-        self.client.session["survey_step"] = 0
+    def test_no_survey_sent(self):
+
+        session = self.client.session
+        session["survey_step"] = None
+        session.save()
+
+        response = self.client.post(
+            reverse("income-message"),
+            data={"Body": ["No"], "From": [str(self.user_phone_number)]},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(SURVEY__DO_NOT_SEND_SURVEY, str(response.content))
+
+    def test_survey_complete(self):
+        session = self.client.session
+        session["survey_step"] = 2
+        session.save()
 
         twilio_response = MessagingResponse()
         twilio_response.message("Survey complete.")
 
-        with patch.object(
-            MessagingResponse, "__str__", return_value=str(twilio_response)
-        ):
-            response = self.client.post(
-                reverse("income-message"),
-                data={
-                    "Body": "answer",
-                    "From": "+1234567890",
-                },
-            )
+        response = self.client.post(
+            reverse("income-message"),
+            data={
+                "Body": ["Last answer to question."],
+                "From": [str(self.user_phone_number)],
+            },
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("Survey complete.", str(response.content))
