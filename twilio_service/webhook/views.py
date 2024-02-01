10b21f504ff1fb0f75af2f1f815a8fab0a19ea3c
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-31 22:22:51 -0500

    Handle is user is known or user resubscribe

--- twilio_service/webhook/views.py
+++ twilio_service/webhook/views.py
@@ -7,32 +7,47 @@ from rest_framework.views import APIView
 from twilio.twiml.messaging_response import MessagingResponse
 
 from accounts.models import UserProfile
-from twilio_service.constant import SURVEY__OPT_OUT_RESPONSE, TWILIO__OPT_OUT
+from twilio_service.constant import SURVEY__CURRENTLY_SUBSCRIBED, SURVEY__OPT_IN_RESPONSE, SURVEY__OPT_OUT_RESPONSE, SURVEY__PREVIOUSLY_SUBSCRIBED, TWILIO__OPT_OUT, TWILIO__UNKNOWN_USER
 from twilio_service.logic.generate_survey_response import GenerateSurveyResponse
 
 
 class TwilioWebhook(APIView):
     def post(self, request, *args, **kwargs):
         try:
+            #TODO - move permission and subscription logic to own file
             twilio_data = parse_qs(request.body.decode("utf-8"))
 
             phone_number = " ".join(twilio_data.get("From", []))
             user_message = " ".join(twilio_data.get("Body", []))
 
             user_profile = UserProfile.objects.get(phone_number=phone_number)
-            user = user_profile.user
 
-            if user_message.lower() == TWILIO__OPT_OUT:
-                user_profile.active = False
-                user_profile.save()
+            if not user_profile: 
+                response = TWILIO__UNKNOWN_USER
 
-                response = SURVEY__OPT_OUT_RESPONSE
-                del request.session["survey_step"]
+            else: 
+                user = user_profile.user
 
-            else:
-                response = GenerateSurveyResponse.get_reponse(
-                    self, request, user, user_message
-                )
+                if not user_profile.active: 
+                    response = SURVEY__PREVIOUSLY_SUBSCRIBED
+
+                elif user_message.lower() == TWILIO__OPT_OUT:
+                    user_profile.active = False
+                    user_profile.save()
+
+                    response = SURVEY__OPT_OUT_RESPONSE
+                    request.session.pop("survey_step", None)
+
+                elif user_message.lower() == TWILIO__OPT_OUT:
+                    user_profile.active = True
+                    user_profile.save()
+
+                    response = SURVEY__OPT_IN_RESPONSE
+
+                else:
+                    response = GenerateSurveyResponse.get_reponse(
+                        self, request, user, user_message
+                    )
 
             twilio_response = MessagingResponse()
             twilio_response.message(response)
