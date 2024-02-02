ef3464e7b3cb0fe2b0fb1534861e03efa0b10f0d
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-02-01 21:09:11 -0500

    Remove websocket until feature can be decided upon

--- twilio_service/urls.py
+++ twilio_service/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from .webhook.views import TwilioWebhook
-from .websocket.consumers import TwilioWebsocket
 
 urlpatterns = [
     path("webhook-survey/", TwilioWebhook.as_view(), name="webhook-survey"),
-    path("websocket-survey/", TwilioWebsocket().as_asgi(), name="websocket-survey"),
 ]
