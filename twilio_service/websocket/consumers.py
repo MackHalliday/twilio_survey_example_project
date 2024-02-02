085ccdd63d35114543b9a97f8b90f5b9e9a3aa17
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-02-01 20:34:10 -0500

    Rename and add print statements - need to add logging

--- twilio_service/websocket/consumers.py
+++ twilio_service/websocket/consumers.py
@@ -3,14 +3,17 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class TwilioConsumer(AsyncWebsocketConsumer):
+class TwilioWebsocket(AsyncWebsocketConsumer):
     async def connect(self):
+        print("connect")
         await self.accept()
 
     async def disconnect(self, close_code):
+        print("disconnect")
         pass
 
     async def receive(self, text_data):
+        print("recieve")
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
